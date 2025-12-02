cb1d5532ac13011375b482d42722e7adc852a06d
ahmadinsekta <ahmad@insekta.co.id>
2025-12-02 22:27:12 +0700

    Update img url

--- client/src/utils/imageUrl.js
+++ client/src/utils/imageUrl.js
@@ -1,6 +1,10 @@
 export const getImageUrl = (path) => {
   if (!path) return "";
 
+  if (!path) {
+    return "https://res.cloudinary.com/dz8dtz5ki/image/upload/v1764597116/user_bsdswt.png";
+  }
+
   // Jika path sudah berupa link lengkap (misal dari Google/Dicebear), biarkan
   if (path.startsWith("http") || path.startsWith("https")) {
     return path;
